62a8250f5891c16d002670458ff702db56e23a86
unknown <Philipp@Chaoscomputer.(none)>
2013-03-25 10:18:38 +0100

    style

--- target/scala-2.9.1/src_managed/main/views/html/main.template.scala
+++ target/scala-2.9.1/src_managed/main/views/html/main.template.scala
@@ -26,21 +26,21 @@ Seq[Any](format.raw/*1.32*/("""
 
 <html>
     <head>
-        <title>"""),_display_(Seq[Any](/*8.17*/title)),format.raw/*8.22*/("""</title>
+        <title>Speiseplan - """),_display_(Seq[Any](/*8.30*/title)),format.raw/*8.35*/("""</title>
         <link rel="stylesheet" media="screen" href=""""),_display_(Seq[Any](/*9.54*/routes/*9.60*/.Assets.at("stylesheets/main.css"))),format.raw/*9.94*/("""">
         <link rel="shortcut icon" type="image/png" href=""""),_display_(Seq[Any](/*10.59*/routes/*10.65*/.Assets.at("images/favicon.png"))),format.raw/*10.97*/("""">
         <script src=""""),_display_(Seq[Any](/*11.23*/routes/*11.29*/.Assets.at("javascripts/jquery-1.7.1.min.js"))),format.raw/*11.74*/("""" type="text/javascript"></script>
     </head>
     <body>
 	
-		<nav>
+		<nav>	
 			<table border="0" frame="vsides" rules="cols" cellpadding="5">
-				<tr>
+				<tr> 
 					<td>
 						<a href="/recipes">Rezepte</a> 
 					</td>
 					<td>
-						<a href="/meals">Essensplan</a>
+						<a href="/meals">Speiseplan</a>
 					</td>
 					<td>
 						"""),_display_(Seq[Any](/*25.8*/form(routes.Application.logout)/*25.39*/ {_display_(Seq[Any](format.raw/*25.41*/("""
@@ -49,9 +49,11 @@ Seq[Any](format.raw/*1.32*/("""
 					</td>
 				</tr>
 			</table>
-		</nav>		
+		</nav>	
 
-		"""),_display_(Seq[Any](/*33.4*/content)),format.raw/*33.11*/("""
+		<hr/>
+
+		"""),_display_(Seq[Any](/*35.4*/content)),format.raw/*35.11*/("""
     </body>
 </html>
 """))}
@@ -66,11 +68,11 @@ Seq[Any](format.raw/*1.32*/("""
 }
                 /*
                     -- GENERATED --
-                    DATE: Sun Mar 24 14:13:30 CET 2013
+                    DATE: Sun Mar 24 21:07:26 CET 2013
                     SOURCE: E:/Eigene Dateien/workspace-scala/essensplan/app/views/main.scala.html
-                    HASH: 77b5848a3c648e6da0fc6fff2fbaa5b6b03a6eca
-                    MATRIX: 509->1|632->31|659->49|746->101|772->106|869->168|883->174|938->208|1035->269|1050->275|1104->307|1165->332|1180->338|1247->383|1560->661|1600->692|1640->694|1727->750|1811->799|1840->806
-                    LINES: 19->1|23->1|24->3|29->8|29->8|30->9|30->9|30->9|31->10|31->10|31->10|32->11|32->11|32->11|46->25|46->25|46->25|48->27|54->33|54->33
+                    HASH: 0f5bd5d5e288c4553f621073315af3b97f59869c
+                    MATRIX: 509->1|632->31|659->49|759->114|785->119|882->181|896->187|951->221|1048->282|1063->288|1117->320|1178->345|1193->351|1260->396|1575->676|1615->707|1655->709|1742->765|1834->822|1863->829
+                    LINES: 19->1|23->1|24->3|29->8|29->8|30->9|30->9|30->9|31->10|31->10|31->10|32->11|32->11|32->11|46->25|46->25|46->25|48->27|56->35|56->35
                     -- GENERATED --
                 */
             
